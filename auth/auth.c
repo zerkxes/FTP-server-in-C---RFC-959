7e92030ddc34fa905b6dad4499e544107d65718e
zerkxes <aniketbose986@gmail.com>
2025-09-27 21:26:56 +0530

    cleaned auth.c

--- auth/auth.c
+++ auth/auth.c
@@ -5,10 +5,12 @@
 #include <unistd.h>
 #include "../logs/log.c"
 
-#define okanon "332 No need account for login."
+#define okAnon "332 No need account for login."
 #define okUser "331 User name okay, password needed."
+#define okPass "230 User logged in, proceed."
 #define invUName "430 Invalid Username or Password."
 #define okNA "202 Command not implemented, superfluous at this site."
+#define invInp "500 Syntax error, command unrecognized and the requested action did not take place. This may include errors such as command line too long."
 #define maxUNameL 50 
 #define maxPlen 100
 #define retry 1
@@ -21,59 +23,75 @@ int Send(int connfd, const char* buff, size_t size){
 }
 
 
-//-1 invalid user, 0 anonymous user, 1 regular user, 2 superuser
-int userAuth(const int connfd){
-    char buff[maxUNameL];
-    char pword[maxPlen];
-    char* inp[2];
-    int user = -1;
-    int read = -1;
-    if((read = recv(connfd, buff, maxUNameL, 0))>0){
-        char *p = strtok(buff, " ");
-        inp[0] = p;
-        p = strtok(NULL, "\n");
-        inp[1] = p;
-        free(p);
-        if(strcmp(inp[0], "USER")!=0){
-            Send(connfd, okNA, strlen(okNA));
-            close(connfd);
+int authHelper(const int connfd, const char* uName){
+    char* getName;
+    size_t nameS = maxUNameL;
+    FILE* fp;
+    int found = 0;
+    if((fp=fopen("users.txt", "r"))==NULL)failureLog("Fatal, unable to read users.txt");
+    
+    while(getline(&getName, &nameS, fp)!=-1){
+        char *p = getName;
+        if(strcmp(strtok(p, " "), uName)==0){
+            found = 1;
+            break;
+        }
+    }
+
+    if(found==0){
+            Send(connfd, invUName, strlen(invUName));
             return -1;
         }
-        char* getName;
-        size_t nameS = 50;
-        FILE* fp;
-        int found = 0;
-        if((fp=fopen("users.txt", "r"))==NULL)failureLog("Fatal, unable to read users.txt");
-        
-        while(getline(&getName, &nameS, fp)!=-1){
-            if(strcmp(strtok(getName, " "), inp[1])==0){
-                found =1;
-                break;
-            }
-        }
-        free(getName);free(fp);
-        if(found){
-            if(strcmp(inp[1], "anonymous")==0 || strcmp(inp[1], "ANONYMOUS")==0)user = 0;
-            Send(connfd, okUser, strlen(okUser));
-            
-            if((read = recv(connfd, pword, maxPlen, 0))<0)failureLog("Unable to read passwword");
-            // PASS *password here*
-            char* p = strtok(pword, " ");
-            if(strcmp(p, "PASS")!=0){
-                Send(connfd, invUName, strlen(invUName));
-                close(connfd);
-                return -1;
-            }
-            user = 1;
-        }
-        else {
-            Send(connfd, invUName, strlen(invUName));
-            user = -1;
-        }
+
+
+    if(strcmp(uName, "anonymous")==0 || strcmp(uName, "ANONYMOUS")==0){
+        Send(connfd, okAnon, strlen(okAnon));
+        return 0;
+    }
+
+    char *pword;
+    if((recv(connfd, pword, maxPlen, 0))<0)failureLog("Unable to read passwword");
+
+    char* p = strtok(pword, " ");
+    if(strcmp(p, "PASS")!=0){
+        Send(connfd, invInp, strlen(invInp));
+        close(connfd);
+        return -1;
+    }
+
+    p = strtok(NULL, "\n ");
+    char *pass = strtok(getName, " ");
+    pass = strtok(NULL, "\n");
+    if(strcmp(p,pass)==0){
+        Send(connfd, okPass, strlen(okPass));
+        return 1;
     }
     else {
         Send(connfd, invUName, strlen(invUName));
-        user = -1;
+        close(connfd);
+        return -1;
     }
+}
+
+
+//-1 invalid user, 0 anonymous user, 1 regular user, 2 superuser
+int userAuth(const int connfd){
+    char buff[maxUNameL];
+    char* inp[2];
+    int user = -1;
+    int read = -1;
+    if((read = recv(connfd, buff, maxUNameL, 0))==-1)failureLog("unable to read");
+
+    char *p = strtok(buff, " ");
+    inp[0] = p;
+    p = strtok(NULL, "\n");
+    inp[1] = p;
+    if(strcmp(inp[0], "USER")!=0){
+        Send(connfd, okNA, strlen(okNA));
+        close(connfd);
+        return -1;
+    }
+    int f = authHelper(connfd, inp[1]);
+
     return user;
 }
